799001b513a244fe08c73530e2de6dc4d40c3dfb
Watchara Kochprapha <kochsarn@yahoo.com>
2019-04-03 12:50:04 +0700

    watchara test Github commit #4

--- SeleniumJavaFramework/src/test/java/demo/GitHubDemo.java
+++ SeleniumJavaFramework/src/test/java/demo/GitHubDemo.java
@@ -4,6 +4,8 @@ public class GitHubDemo {
  public static void main(String[] args) {
 	System.out.println("Watchara Test for Git commit#2");
 	System.out.println("Watchara Test for Git commit#3");
+	System.out.println("Watchara Test for Git commit#4");
+	
 	
 }
 	
